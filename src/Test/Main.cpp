46add92a19f7950925ebacc759802f63ac3242fa
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-03-27 21:59:46 +0200

    Translated testing messages

--- src/Test/Main.cpp
+++ src/Test/Main.cpp
@@ -15,14 +15,8 @@ using rgb = unsigned char;
 int main(int anzahl, char* args[]) {
     cout << "Hello World" << endl;
 
-    /*cout << "Teste Portable File Dialogs" << endl;
-    pfd::notify("PFD Test", "Dies ist eine Benachrichtigung", pfd::icon::warning);
-    pfd::message("PFD Test", "Dies ist eine MessageBox", pfd::choice::abort_retry_ignore, pfd::icon::question);*/
-
-    cout << "Teste stb-image" << endl;
+    cout << "Testing stb-image" << endl;
     cout << string(50, '-') << endl;
-
-    cout << "Starte Test" << endl;
 
     const int width = 1000, height = 1000, channels = 3, lineCount = 5;
     const int size = width * height * channels;
@@ -31,52 +25,52 @@ int main(int anzahl, char* args[]) {
     string normal = "random.png", dark = "randomDark.png";
     gen.seed(system_clock::now().time_since_epoch().count());
 
-    cout << "Generiere Randomisierte Pixeldaten" << endl;
+    cout << "Generating random color values" << endl;
     for (int i = 0; i < size; i++) {
         if (i < width * channels * lineCount) pixels[i] = (rgb) 0xFF;
         else pixels[i] = (rgb) gen() % 256;
     }
 
-    cout << "Speichere Bild";
+    cout << "Saving image";
     int result = stbi_write_png(normal.c_str(), width, height, channels, pixels, width * channels);
     if (result) {
-        cout << " -- Erfolgreich" << endl;
+        cout << " -- success" << endl;
     } else {
-        cout << " -- Fehlgeschlagen. Breche weiteren Test ab" << endl;
+        cout << " -- error. Quitting" << endl;
         return result;
     }
 
-    cout << "Lade gelesenes Bild wieder aus";
+    cout << "Load written image";
     int w, h, c;
     rgb* read = stbi_load(normal.c_str(), &w, &h, &c, channels);
 
     if (read == NULL || w != width || h != height || c != channels) {
-        cout << " -- Fehlgeschlagen. Breche weiteren Test ab" << endl;
+        cout << " -- error. Quitting" << endl;
         return result;
     } else {
-        cout << " -- Erfolgreich" << endl;
+        cout << " -- success" << endl;
     }
 
-    cout << "Teste nach weißen Reihen";
+    cout << "Test for white lines";
     bool testLines = true;
     for (int i = 0; i < width * channels * lineCount; i++) if (read[i] != 0xFF) testLines = false;
 
-    if (testLines) cout << " -- Erfolgreich" << endl;
-    else cout << " -- Fehlgeschlagen" << endl;
+    if (testLines) cout << " -- success" << endl;
+    else cout << " -- error" << endl;
 
-    cout << "Halbiere Werte" << endl;
+    cout << "Dividing values by 2" << endl;
     for (int i = 0; i < size; i++) read[i] /= 2;
 
-    cout << "Flippe und schreibe dunkleres Bild";
+    cout << "Flipping darker image and saving it again";
     stbi_flip_vertically_on_write(true);
     result = stbi_write_png(dark.c_str(), width, height, channels, read, width * channels);
 
-    if (result) cout << " -- Erfolgreich" << endl;
-    else cout << " -- Fehlgeschlagen" << endl;
+    if (result) cout << " -- success" << endl;
+    else cout << " -- error" << endl;
 
-    cout << "Lösche gelesene Daten" << endl;
+    cout << "Deleting read data" << endl;
     stbi_image_free(read);
 
-    cout << "Test erfolgreich abgeschlossen" << endl;
+    cout << "Test finished" << endl;
     cout << string(50, '-') << endl;
 }
