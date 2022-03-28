409001608e9ae4c384173575c3e8ea42676b1dcd
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-03-28 14:38:23 +0200

    Preparing Animation Test

--- src/Test/Animation/AnimTest.hpp
+++ src/Test/Animation/AnimTest.hpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <string>
+
+#include "PixelRenderer/Renderer.hpp"
+#include "PixelRenderer/Font.hpp"
+#include "PixelRenderer/Texture.hpp"
+
+Renderer* renderer;
+FontManager* fontMan;
+TextureManager* textureMan;
+
+std::string getFilename(const int& image);
