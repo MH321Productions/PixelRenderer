fbb58d56b82443ca0e0821818fe2047c15fc8a2a
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-03-31 12:47:43 +0200

    Added filling rects to animation

--- src/Test/Animation/Anim1Shapes.cpp
+++ src/Test/Animation/Anim1Shapes.cpp
@@ -1,10 +1,25 @@
 #include "AnimTest.hpp"
 #include "PixelRenderer/Unicode.hpp"
+#include "PixelRenderer/Geometry.hpp"
 
-static String32 description = U"Render basic shapes";
+String32 description = U"Render basic shapes";
+Rect fill, lineRect;
+int fillOffset, lineRectOffset, lineRectSize;
 
 void AnimationTest::renderShapes(const int& image) {
     renderer->setColor(Colors::Green);
     renderer->setBlendingMethod(BlendingMethod::ColorBlending);
     renderer->drawText(robotoFont, description, 80, 150, 180);
+    renderer->setBlendingMethod(BlendingMethod::NoBlending);
+
+    //Fill rect
+    renderer->setColor(Colors::Red);
+    fillOffset = (int) (Functions::sin60(image) * 100);
+    fill = {120, 300, 200 + fillOffset, 200 + 2 * fillOffset};
+    renderer->fillRect(fill);
+
+    //TODO: Draw line rect
+    //renderer->setColor(Colors::Yellow);
+
+    //TODO: Render other shapes when they are implemented
 }
