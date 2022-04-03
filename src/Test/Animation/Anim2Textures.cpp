aeeb89a7b8c8ea284193376d2e9184f32d44e098
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-04-04 00:31:24 +0200

    Added table description

--- src/Test/Animation/Anim2Textures.cpp
+++ src/Test/Animation/Anim2Textures.cpp
@@ -3,6 +3,7 @@
 #include "PixelRenderer/Geometry.hpp"
 
 String32 texDescription = U"Render and scale textures";
+String32 texFile = U"File", texMem = U"RAM", texLinear = U"Linear scaling", texNearest = U"Nearest scaling";
 Rect texTableVertLeft {200, 130, 10, 950};
 Rect texTableVertRight {1060, 130, 10, 950};
 Rect texTableHorTop {0, 210, 1920, 10};
@@ -13,6 +14,10 @@ void AnimationTest::renderTextures(const int& image) {
     renderer->setColor(Colors::Green);
     renderer->setBlendingMethod(BlendingMethod::ColorBlending);
     renderer->drawText(robotoFont, texDescription, 20, 120, 150);
+    renderer->drawText(robotoFont, texFile, 50, 440, 60);
+    renderer->drawText(robotoFont, texMem, 35, 910, 60);
+    renderer->drawText(robotoFont, texLinear, 420, 190, 60);
+    renderer->drawText(robotoFont, texNearest, 1275, 190, 60);
 
     //create table
     renderer->setColor(Colors::Red);
