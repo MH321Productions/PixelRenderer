f63b4f71eafa39d58e1583ac5c0e37e74b823b09
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-04-24 23:52:20 +0200

    Changed cross image to fruits image

--- src/Test/Animation/Anim2Textures.cpp
+++ src/Test/Animation/Anim2Textures.cpp
@@ -6,7 +6,8 @@ String32 texDescription = U"Render and scale textures";
 String32 texFile = U"File", texMem = U"Memory";
 Rect texTableVert {955, 130, 10, 950};
 Rect texTableHor {0, 270, 1920, 10};
-Rect texCross {10, 290, 18, 18};
+Rect texFruitDest {10, 290, 935, 780};
+Rect texFruitSrc {491, 0, 3913, 3264};
 Rect texCheck {975, 290, 18, 18};
 
 void AnimationTest::renderTextures(const int& image) {
@@ -24,27 +25,20 @@ void AnimationTest::renderTextures(const int& image) {
 
     //render images
     double scaleX, scaleY;
-    int offsetX, offsetY;
 
+    //file
+    renderer->drawTexture(cross, texFruitSrc, texFruitDest);
+
+    //memory
     if (image < 30) {
         scaleX = Functions::linUp(image, 30) * 910;
         scaleY = scaleX / 3.0;
-        offsetX = (int) scaleX;
-        offsetY = (int) scaleY;
+        texCheck.width = (int) scaleX;
+        texCheck.height = (int) scaleY;
     } else {
         scaleY = Functions::linUp(image - 30, 30) * 456.66666666666666 + 303.3333333333333;
-        offsetX = 910;
-        offsetY = (int) scaleY;
+        texCheck.width = 910;
+        texCheck.height = (int) scaleY;
     }
-
-    //file
-    texCross.width = 18 + offsetX;
-    texCross.height = 18 + offsetY;
-    renderer->drawTexture(cross, Rect::emptyRect, texCross);
-
-
-    //memory
-    texCheck.width = 21 + offsetX;
-    texCheck.height = 20 + offsetY;
     renderer->drawTexture(check, Rect::emptyRect, texCheck);
 }
