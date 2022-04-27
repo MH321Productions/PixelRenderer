e7e0aa8f86eb85522920cc904358d520dca7e9ba
Mika Herrmann <nh273@stud.uni-heidelberg.de>
2022-04-27 09:28:34 +0200

    First spritesheet test version

--- src/Test/Animation/Anim6Sprite.cpp
+++ src/Test/Animation/Anim6Sprite.cpp
@@ -2,9 +2,13 @@
 
 #include "AnimTest.hpp"
 #include "AnimPart.hpp"
+#include "PixelRenderer/Geometry.hpp"
 
 using namespace std;
 
+SpriteInfo info(400, 400, 10, 4);
+Rect animDest(100, 100, 400, 400);
+
 void PartSprite::renderPart(const int& image) {
-    
+    anim->renderer->drawSpritesheet(anim->sprite, animDest, info, image / 2, true);
 }
